7ec9489f9faaed5b60cf6d67cdc97b4253c9b2bb
Daniel Bradley <87danielbradley@gmail.com>
2020-07-23 02:03:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,8 @@
 
 import numpy as np
 import pandas as pd
-import datetime as dt
 
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+
 import numpy as np
 import pickle
 
